4ce2293f563edfd1b9e742d9f445a97e7ce0c7a9
obriematt <matthewo@pdx.edu>
2016-07-10 17:07:31 -0700

    Testing RNG files

--- rng/hashrng.py
+++ rng/hashrng.py
@@ -10,13 +10,28 @@
 
 
 import hashlib
+from bitstream import BitStream
+from numpy import *
+import matplotlib.pyplot as plt
+# import scipy import stats
+
 
 
 def hashrng():
-    n = hashlib.md5("w".encode('utf-8')).hexdigest()
-    m = hashlib.md5("whateve your string is".encode('utf-8')).hexdigest()
+    n = hashlib.sha1("w".encode('utf-8')).hexdigest()
+    m = hashlib.sha1("whateve your string is".encode('utf-8')).hexdigest()
     i = int(m,16)
     j = int(n,32)
+    stream = BitStream()
+    stream.write("asdqwevjvwevwefovvwl", str)
+    total = len(stream)
+    one_count = stream.count('1')
+    zero_count = stream.count('0')
+    one_count /= total
+    zero_count /= total
+    print(one_count)
+    print(zero_count)
+    print(stream)
     print(i)
     print(j)
 
