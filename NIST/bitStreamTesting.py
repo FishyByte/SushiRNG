50ea91b80cc7bd1b627c864a25b55e7913e445e5
c-asakawa <chris@asakawa.me>
2016-07-13 11:26:31 -0700

    changed params for new data, removed redundant code

--- NIST/bitStreamTesting.py
+++ NIST/bitStreamTesting.py
@@ -1,17 +1,15 @@
-#Corey Aing
-#07/12/2016
-#Python script to run NIST tests against a bitstream found in a textfile
+# Corey Aing
+# 07/12/2016
+# Python script to run NIST tests against a bitstream found in a textfile
 
-import subprocess
 from subprocess import Popen, PIPE
 
-#For now this will just perform a Runs test on 120 binary bits from data/data.pi
-#Results and statistics .txt files are found in ./experiments/AlgorithmTesting/Runs
-p = Popen(["./assess", "120"], stdin=PIPE, stdout=PIPE)
+# Results and statistics .txt files are found in ./experiments/AlgorithmTesting/Runs
+p = Popen(["./assess", "100093"], stdin=PIPE, stdout=PIPE)
 
 p.stdin.write("0\n")
-p.stdin.write("data/data.pi\n")
+p.stdin.write("data/fishBits.txt\n")
 p.stdin.write("0\n")
-p.stdin.write("000100000000000\n")
+p.stdin.write("111111111111111\n")
 p.stdin.write("1\n")
 p.stdin.write("0\n")
