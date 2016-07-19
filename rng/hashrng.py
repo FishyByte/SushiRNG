4774db5766a98f54983275806c130c490d1c1eec
Matt O'Brien <matthewo@pdx.edu>
2016-07-18 22:36:09 -0700

    Made into class

--- rng/hashrng.py
+++ rng/hashrng.py
@@ -16,180 +16,182 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
+class rng_pool():
 
-def create_test_list(num, size):
-    bit_list = np.zeros(size, dtype=int)
-    for i in range(len(bit_list)):
-        if i % num == 0:
-            bit_list[i] = 1
+    def create_test_list(self, num, size):
+        bit_list = np.zeros(size, dtype=int)
+        for i in range(len(bit_list)):
+            if i % num == 0:
+                bit_list[i] = 1
 
-    return bit_list
-
-
-# Stir functions
-def stir_pool(entropy_one, entropy_two):
-
-    # Create new numpy list XOR from two
-    new_entropy_pool = np.logical_xor(entropy_one,entropy_two)
-
-    return new_entropy_pool
+        return bit_list
 
 
-# Calculate the distributions of 1s and 0s in total string.
-def dist_calculations(bit_list):
+    # Stir functions
+    def stir_pool(self, entropy_one, entropy_two):
 
-    one_count = np.count_nonzero(bit_list)
-    total_length = len(bit_list)
-    zero_count = total_length - one_count
+        # Create new numpy list XOR from two
+        new_entropy_pool = np.logical_xor(entropy_one,entropy_two)
 
-    percent_one = one_count / float(total_length)
-    percent_zero = zero_count / float(total_length)
+        return new_entropy_pool
 
 
-    return percent_one, percent_zero
+    # Calculate the distributions of 1s and 0s in total string.
+    def dist_calculations(self, bit_list):
+
+        one_count = np.count_nonzero(bit_list)
+        total_length = len(bit_list)
+        zero_count = total_length - one_count
+
+        percent_one = one_count / float(total_length)
+        percent_zero = zero_count / float(total_length)
 
 
-# Entropy calculations
-def entropy_calculations(percent_one, percent_zero):
-
-    # calculating the bits of entropy
-    entropy = (-percent_one*math.log(percent_one,2))+(-percent_zero*math.log(percent_zero,2))
-    # entropy = stats.entropy(percent_one) + stats.entropy(percent_zero)
-
-    return entropy
+        return percent_one, percent_zero
 
 
-# I need to fix this.
-# Find the corrected length of bits given entropy calculations
-def entropy_correction(entropy):
-    corrected_bits = math.ceil(4096 * entropy)
-    corrected_bits = int(corrected_bits)
-    corrected_bits = 4096 + (4096-corrected_bits)
-    return corrected_bits
+    # Entropy calculations
+    def entropy_calculations(self, percent_one, percent_zero):
+
+        # calculating the bits of entropy
+        entropy = (-percent_one*math.log(percent_one,2))+(-percent_zero*math.log(percent_zero,2))
+        # entropy = stats.entropy(percent_one) + stats.entropy(percent_zero)
+
+        return entropy
 
 
-# Grab the needed amounts of bits to ensure 128 bits of entropy
-def alter_bit_length(bit_list, corrected_bits):
-    # new_bit_list = np.zeros(corrected_bits)
-    # for i in range(corrected_bits):
-        # new_bit_list[i] = bit_list[i]
-    new_bit_list = create_test_list(5, corrected_bits)
-    return new_bit_list
+    # I need to fix this.
+    # Find the corrected length of bits given entropy calculations
+    def entropy_correction(self, entropy):
+        corrected_bits = math.ceil(4096 * entropy)
+        corrected_bits = int(corrected_bits)
+        corrected_bits = 4096 + (4096-corrected_bits)
+        return corrected_bits
 
 
-# Whitener for the 128 bit list to generate a random number
-def whiten_numbers(min_value,max_value, bit_list,whitener):
-
-    # Stringify the list
-    bit_string = np.array2string(bit_list)
-
-    # Update the hash libraray
-    whitener.update(bit_string)
-
-    # Digest the library and convert into a int 32
-    hash_number = whitener.hexdigest()
-    hash_number = int(hash_number,32)
-
-    # Kick out number of no more than max_value
-    random_number = hash_number % max_value
-    return random_number
+    # Grab the needed amounts of bits to ensure 128 bits of entropy
+    def alter_bit_length(self, bit_list, corrected_bits):
+        # new_bit_list = np.zeros(corrected_bits)
+        # for i in range(corrected_bits):
+            # new_bit_list[i] = bit_list[i]
+        new_bit_list = self.create_test_list(5, corrected_bits)
+        return new_bit_list
 
 
-# Report function for all of the number's information
-def report_stats(bit_list, whitener):
+    # Whitener for the 128 bit list to generate a random number
+    def whiten_numbers(self, min_value,max_value, bit_list,whitener):
 
-    # Call Dist function
-    percent_one, percent_zero = dist_calculations(bit_list)
+        # Stringify the list
+        bit_string = np.array2string(bit_list)
 
-    # Call Entropy
-    entropy = entropy_calculations(percent_one, percent_zero)
+        # Update the hash libraray
+        whitener.update(bit_string)
 
-    # Correct bits
-    correct_bits = entropy_correction(entropy)
+        # Digest the library and convert into a int 32
+        hash_number = whitener.hexdigest()
+        hash_number = int(hash_number,32)
 
-    new_bit_list = alter_bit_length(bit_list,correct_bits)
-
-    random_number = whiten_numbers(0,20, new_bit_list,whitener)
-
-    # print(len(new_bit_list))
-    # print "Percent of 1s:", percent_one
-    # print "Percent of 0s:", percent_zero
-    # print "Bits of Entropy:", entropy
-    print "Random number is:", random_number
-
-    return random_number
+        # Kick out number of no more than max_value
+        random_number = hash_number % max_value
+        return random_number
 
 
-# 8-Ball Responses
-def eight_ball_response(number):
+    # Report function for all of the number's information
+    def report_stats(self, bit_list, whitener):
 
-    if number == 0:
-        response = "It is Certain"
-    elif number == 1:
-        response = "It is decidedly so"
-    elif number == 2:
-        response = "Without a doubt"
-    elif number == 3:
-        response = "Yes, definitely"
-    elif number == 4:
-        response = "You may rely on it"
-    elif number == 5:
-        response = "As I see it, yes"
-    elif number == 6:
-        response = "Most likely"
-    elif number == 7:
-        response = "Outlook good"
-    elif number == 8:
-        response = "Yes"
-    elif number == 9:
-        response = "Signs point to yes"
-    elif number == 10:
-        response = "Reply hazy, try again"
-    elif number == 11:
-        response = "Ask again later"
-    elif number == 12:
-        response = "Better not tell you now"
-    elif number == 13:
-        response = "Cannot predict now"
-    elif number == 14:
-        response = "Concentrate and ask again"
-    elif number == 15:
-        response = "Don't count on it"
-    elif number == 16:
-        response = "My reply is no"
-    elif number == 17:
-        response = "My sources say no"
-    elif number == 18:
-        response = "Outlook not so good"
-    elif number == 19:
-        response = "Very doubtful"
-    else:
-        response = "Lol no"
+        # Call Dist function
+        percent_one, percent_zero = self.dist_calculations(bit_list)
 
-    return response
+        # Call Entropy
+        entropy = self.entropy_calculations(percent_one, percent_zero)
+
+        # Correct bits
+        correct_bits = self.entropy_correction(entropy)
+
+        new_bit_list = self.alter_bit_length(bit_list,correct_bits)
+
+        random_number = self.whiten_numbers(0,20, new_bit_list,whitener)
+
+        # print(len(new_bit_list))
+        # print "Percent of 1s:", percent_one
+        # print "Percent of 0s:", percent_zero
+        # print "Bits of Entropy:", entropy
+        print "Random number is:", random_number
+
+        return random_number
+
+
+    # 8-Ball Responses
+    def eight_ball_response(self, number):
+
+        if number == 0:
+            response = "It is Certain"
+        elif number == 1:
+            response = "It is decidedly so"
+        elif number == 2:
+            response = "Without a doubt"
+        elif number == 3:
+            response = "Yes, definitely"
+        elif number == 4:
+            response = "You may rely on it"
+        elif number == 5:
+            response = "As I see it, yes"
+        elif number == 6:
+            response = "Most likely"
+        elif number == 7:
+            response = "Outlook good"
+        elif number == 8:
+            response = "Yes"
+        elif number == 9:
+            response = "Signs point to yes"
+        elif number == 10:
+            response = "Reply hazy, try again"
+        elif number == 11:
+            response = "Ask again later"
+        elif number == 12:
+            response = "Better not tell you now"
+        elif number == 13:
+            response = "Cannot predict now"
+        elif number == 14:
+            response = "Concentrate and ask again"
+        elif number == 15:
+            response = "Don't count on it"
+        elif number == 16:
+            response = "My reply is no"
+        elif number == 17:
+            response = "My sources say no"
+        elif number == 18:
+            response = "Outlook not so good"
+        elif number == 19:
+            response = "Very doubtful"
+        else:
+            response = "Lol no"
+
+        return response
 
 
 # Main function for testing
 def main():
 
+    new_pool = rng_pool()
     whitener = hashlib.sha1()
     # Make the testing list
-    bit_list = create_test_list(3, 4096)
-    second_bit_list = create_test_list(10, 4096)
+    bit_list = new_pool.create_test_list(3, 4096)
+    second_bit_list = new_pool.create_test_list(10, 4096)
 
     # Report information
     # report_stats(bit_list,whitener)
 
-    new_entropy_pool = stir_pool(bit_list,second_bit_list)
+    new_entropy_pool = new_pool.stir_pool(bit_list,second_bit_list)
 
     # report_stats(new_entropy_pool,whitener)
 
-    new_entropy_pool = stir_pool(new_entropy_pool, bit_list)
+    new_entropy_pool = new_pool.stir_pool(new_entropy_pool, bit_list)
 
 
     for i in range(5):
-        input = report_stats(new_entropy_pool, whitener)
-        response = eight_ball_response(input)
+        input = new_pool.report_stats(new_entropy_pool, whitener)
+        response = new_pool.eight_ball_response(input)
         print response
 
 main()
