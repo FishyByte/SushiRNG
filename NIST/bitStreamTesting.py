06164e6f08217ae4ca1a132401c140045206dca4
Corey Aing <caing@pdx.edu>
2016-07-20 13:42:20 -0700

    updated .gitignore

--- NIST/bitStreamTesting.py
+++ NIST/bitStreamTesting.py
@@ -5,7 +5,6 @@
 import subprocess
 from subprocess import Popen, PIPE
 
-#For now this will just perform a Runs test on 120 binary bits from data/data.pi
 #Results and statistics .txt files are found in ./experiments/AlgorithmTesting/Runs
 #The number following the ./assess argument states the size of the bitstream
 try:
@@ -13,8 +12,8 @@ try:
 
 	p.stdin.write("0\n")#indicates that we want to select a file
 	p.stdin.write("data/fishBits.txt\n")#indicates the file path
-	p.stdin.write("0\n")#indicates that we want to select a subset of tests to run
-	p.stdin.write("100000000000000\n")#indicates to run the 4th test of 15 total
+	p.stdin.write("1\n")#apply all tests
+	p.stdin.write("0\n")#indicates using default parameters
 	p.stdin.write("1\n")#indicates how many repeated tests against the bitstream size
 	#as long as the .txt file has enough bits
 	p.stdin.write("0\n")#indicates its an ASCII binary file consisting of 0's and 1's
