ce4f0f730add8a9c3db7a5217fd993f3b00063ed
c-asakawa <chris@asakawa.me>
2016-07-20 00:11:31 -0700

    created more testing scripts

--- rasPi/generate_nist_data.py
+++ rasPi/generate_nist_data.py
@@ -6,14 +6,16 @@ fish_stream = FishStream()
 fish_pool = FishPool()
 
 # output bitstream to file
-test_input = open('../NIST/data/fishBits09.txt', 'r')
+test_input1 = open('../NIST/data/fishBits09.txt', 'r')
+test_input2 = open('../NIST/data/fishBits10.txt', 'r')
+test_input3 = open('../NIST/data/fishBits11.txt', 'r')
+test_input4 = open('../NIST/data/fishBits12.txt', 'r')
+
 test_output = open('fishData/fishBits.txt', 'w')
 test_output.truncate()
 
 print_count = 0
-
-
-input_bits = test_input.read()
+input_bits = test_input1.read() + test_input2.read() + test_input3.read() + test_input4.read()
 input_bits.replace(' ', '')
 input_bits.rstrip('\n')
 for current in input_bits:
@@ -33,11 +35,12 @@ while True:
     entropy = fish_pool.entropy_calculations(prob1, prob2)
     correct_bits = int(fish_pool.entropy_correction(entropy))
     bit_list = str(fish_stream.get_bits(correct_bits))
-    result = fish_pool.whiten_numbers(0, 257, bit_list)
+    result = fish_pool.whiten_numbers(0, 2, bit_list)
     test_output.write(str(result))
-    test_output.write(', ')
     print_count += 1
+    if print_count % 64 == 0:
+        test_output.write('\n')
 
 
-
+test_output.write('\n')
 print print_count
