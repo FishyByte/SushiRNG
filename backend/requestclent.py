0043b8ff63c932b5a28c240ba41e44facbcb0c03
Nick McHale <nmchale@pdx.edu>
2016-07-23 00:53:25 -0700

    uploads work, get requests 2morrrow

--- backend/requestclent.py
+++ backend/requestclent.py
@@ -24,18 +24,12 @@
 import requests
 import binascii
 import os
-# 64 bits here
-input_data = '1100110010101000100010011101101100111100101000110110000111100111'
-encoded = int(input_data, 2)
-# encoded = binascii.unhexlify('%x' % encoded)
-# print encoded
-# print bin(int(binascii.hexlify(encoded), 16))[2:]
 
-
-payload = {
-    'pub_key': '233taewdzvx',
-    'fishBits': encoded
+headers = {
+    'number-bytes-requested': 10
 }
 print os.getcwd()
-r = requests.post('http://127.0.0.1:5000/add-bytes', data=payload, files={'file': open('/home/nick/SushiRNG/backend/bits.bin','rb')})
+r = requests.get('http://127.0.0.1:5000/getBytes', headers=headers)
 print r.status_code
+print r.headers
+print r.text
