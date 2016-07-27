313dde6249d7c10a9c2cff9787c55a0ddf572043
c-asakawa <chris@asakawa.me>
2016-07-27 00:07:57 -0700

    refactored 8ball into its own file

--- FishFate/www/js/app.js
+++ FishFate/www/js/app.js
@@ -26,9 +26,7 @@ app.run(function ($ionicPlatform) {
 
 app.controller('fishController', function ($scope, $ionicHistory) {
   var optionOpen = [false, false, false];
-  var answers = [
-    'do or do not,<br>there is<br>no try'
-  ];
+
 
 
   $scope.images = {
@@ -60,45 +58,6 @@ app.controller('fishController', function ($scope, $ionicHistory) {
   $scope.selectCoinFlip  = function () { showCoinFlip();  };
 
 
-  $scope.eightBall = {
-    answer: answers[0]
-  };
-
-  $scope.submitEightBall = function () {
-    animateEightBall();
-  };
-
-
-
-  /* animations */
-  function animateEightBall() {
-    var triangle = $('#triangle');
-    var answer = $('#answer');
-    var eightBall = $('#eightBall01');
-
-    triangle.fadeOut('fast');
-    answer.fadeOut('fast');
-
-    eightBall.animate({
-      right: '160px'
-    }, 300, function (){
-      eightBall.animate({
-        right: '140px'
-      }, 300, function(){
-        eightBall.animate({
-          right: '160px'
-        }, 300, function(){
-          eightBall.animate({
-            right: '150px'
-          }, 300 ,function (){
-            triangle.fadeIn('slow');
-            answer.fadeIn('slow');
-          });
-
-        });
-      });
-    });
-  }
 
   /**
    *  JQuery effects
--- FishFate/www/js/eightBallController.js
+++ FishFate/www/js/eightBallController.js
@@ -1,3 +1,52 @@
 /**
  * Created by asakawa on 7/27/16.
  */
+var app = angular.module('FishFate');
+app.controller('eightBallController', function ($scope) {
+
+
+  var answers = [
+    'do or do not,<br>there is<br>no try'
+  ];
+
+
+  $scope.eightBall = {
+    answer: answers[0]
+  };
+
+  $scope.submitEightBall = function () {
+    animateEightBall();
+  };
+
+
+
+  /* animations */
+  function animateEightBall() {
+    var triangle = $('#triangle');
+    var answer = $('#answer');
+    var eightBall = $('#eightBall01');
+
+    triangle.fadeOut('fast');
+    answer.fadeOut('fast');
+
+    eightBall.animate({
+      right: '160px'
+    }, 300, function (){
+      eightBall.animate({
+        right: '140px'
+      }, 300, function(){
+        eightBall.animate({
+          right: '160px'
+        }, 300, function(){
+          eightBall.animate({
+            right: '150px'
+          }, 300 ,function (){
+            triangle.fadeIn('slow');
+            answer.fadeIn('slow');
+          });
+
+        });
+      });
+    });
+  }
+});
