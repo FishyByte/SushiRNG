491c854803892a28acbcd882b168f3af6292ce5a
c-asakawa <chris@asakawa.me>
2016-07-28 10:31:01 -0700

    added dynamic bit calcuations

--- app.py
+++ app.py
@@ -28,14 +28,15 @@ from werkzeug.utils import secure_filename
 from bitstream import BitStream
 from numpy import *
 import binascii
+import math
 import os
 from datetime import datetime
 import time
 
 UPLOAD_FOLDER = 'data'
 ALLOWED_EXTENSIONS = ['bin']
-MAX_REQUEST_SIZE = 1000  # users may request up to 1MB
-MAX_INT_RANGE = 2147483647
+MAX_REQUEST_SIZE = 1000     # users may request up to 1MB
+MAX_INT_RANGE = 2147483647  # max value for an integer
 
 app = Flask(__name__)
 CORS(app)
@@ -105,36 +106,26 @@ def get_bits():
 def get_ints():
     quantity = int(request.headers.get('quantity'))
     max_value = int(request.headers.get('max-value'))
-    # byte_mult = get_byte_multiplier(max_value)
-    # bounds_check = quantity * byte_mult
+    bits_requested = get_number_bits(max_value)
+
     respond = ''
 
-    # error checking for days
-    # if quantity is None:
-    #     return abort(400)
-    # if max_value is None:
-    #     return abort(400)
-    # if quantity < 1:
-    #     return abort(400)
-    # if bounds_check > MAX_REQUEST_SIZE:
-    #     return abort(400)
-    # if (len(myBitStream) / 8) < bounds_check:
-    #     return abort(400)
-    # if max > MAX_INT_RANGE:
-    #     return abort(400)
+    # empty request
+    if quantity is None or max_value is None:
+        return abort(400)
+
+    # param value to small
+    if quantity < 1 or max_value < 2:
+        return abort(400)
+
+    # limit request size to 1000 bytes/8000 bits
+    if bits_requested > (MAX_REQUEST_SIZE * 8):
+        return abort(400)
+
+    # too large of a range
+    if max_value > MAX_INT_RANGE:
+        return abort(400)
     try:
-
-        # i know this is dumb.. just want to get it working
-        if max_value < 8:       # 111 = 7
-            number_bits = 3
-        elif max_value < 16:    # 1111 = 15
-            number_bits = 4
-        elif max_value < 32:     # 11111 = 31
-            number_bits = 4
-        else:
-            number_bits = 5
-
-
         # loop till we fill the order
         while True:
             # ship it
@@ -142,25 +133,34 @@ def get_ints():
                 return respond
 
             # grab some byte(s) for one value
-            current = int(str(myBitStream.read(number_bits)), 2)
+            current = int(str(myBitStream.read(bits_requested)), 2)
 
             # within range? add to return string
-            if current < max:
+            if current < max_value:
                 respond += str(current) + ' '  # white space delimiter
                 quantity -= 1
+            # lets not be wasteful, write unused value back to stream
+            else:
+                myBitStream.write(str(current))
 
     except Exception, e:
         print e
         return abort(500)
 
 
-def get_byte_multiplier(int_value):
-    counter = 1
+# calculate the required number of bits
+def get_number_bits(upper_bound):
+    exponent = 1
+    number_bits = 1
     while True:
-        if int_value < 256:  # one bye is 0-255
-            return counter
-        int_value -= 256
-        counter += 1
+
+        if upper_bound >= pow(2, exponent):  # 2, 4, 8, 16
+            number_bits += 1
+        else:
+            print '**********', 'number of bits:', number_bits, '************'  # testing
+            return number_bits
+
+        exponent += 1
 
 
 # ********************************************************
