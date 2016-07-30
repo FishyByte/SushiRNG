5c02aaeddf5db4cc9357b6069e9c41de594ee60c
c-asakawa <chris@asakawa.me>
2016-07-29 19:08:35 -0700

    implmented menu hide controller

--- FishFate/www/js/about.js
+++ FishFate/www/js/about.js
@@ -2,20 +2,19 @@
  * Created by asakawa on 7/29/16.
  */
 var app = angular.module('FishFate');
-app.controller('menuOptionsController', function ($scope) {
-  var listItems = [ $('#listItem0'), $('#listItem1'), $('#listItem2'), $('#listItem3'), $('#listItem4'),
-  ];
-  $scope.clickListItem = function(index){
-    hideAllItems();
+app.controller('aboutViewController', function ($scope) {
+  /* array to hold all the jQuery elements */
+  var listItems = [ $('#listItem0'), $('#listItem1'), $('#listItem2'), $('#listItem3'), $('#listItem4') ];
 
-  }
+  /* this click function is called from the about view */
+  $scope.clickListItem = function(index){  hideAllItems( showOneItem(index) );  };
+
   function hideAllItems (callback, index) {
     for (var i = 0; i < listItems.length; i++){
       listItems[i].fadeOut('fast');
       callback(index);
     }
   }
-  function showOneItem(index){
-    listItems[index].fadeIn('fast');
-  }
+  /* helper function to show just one element */
+  function showOneItem(index){ listItems[index].fadeIn('fast'); }
 });
