2db49c4d21183513ab2a0e210a4a88c898253a2f
c-asakawa <chris@asakawa.me>
2016-07-29 21:05:55 -0700

    added functionality to get binary string

--- FishFate/www/js/random.js
+++ FishFate/www/js/random.js
@@ -4,12 +4,20 @@
 
 var app = angular.module('FishFate');
 app.controller('randomController', function ($scope, $http, $ionicPopup) {
+  /* used for formatting the pop up responses */
+  var titleArray = [
+    'The fish retrieved this number for you',
+    'The fish retrieved this binary string for you'
+  ];
+
+
 
   $scope.randoms = {
     getInt: {
       maxValue: '255',
       response: ''
-    }
+    },
+    getBinary: { quantity: '20' }
   };
   // When button is clicked, the popup will be shown...
   $scope.getInt = function () {
@@ -26,24 +34,44 @@ app.controller('randomController', function ($scope, $http, $ionicPopup) {
      }).then(function successCallback(response) {
         console.log('max:', $scope.randoms.getInt.maxValue, '   returned:', response.data);
         $scope.randoms.getInt.response = response.data;
-        popUpResponse();
+        popUpResponse(0);
 
      }, function errorCallback(response) {
      console.log(response);
      });
-
-
-
-
   };
 
 
-  var popUpResponse = function () {
+    $scope.getBinary = function () {
+
+     delete $http.defaults.headers.common['X-Requested-With'];
+     $http({
+     method: "GET",
+     url: 'https://fish-bit-hub.herokuapp.com/get-binary',
+     headers: {
+     'quantity': $scope.randoms.getBinary.quantity
+     },
+     crossDomain: true
+     }).then(function successCallback(response) {
+        console.log('max:', $scope.randoms.getInt.maxValue, '   returned:', response.data);
+        $scope.randoms.getInt.response = response.data;
+        popUpResponse(1);
+
+     }, function errorCallback(response) {
+     console.log(response);
+     });
+  };
+
+
+
+
+  var popUpResponse = function (index) {
+
     // Custom popup
     var myPopup = $ionicPopup.prompt({
       template: '<h1 style="text-align: center">{{randoms.getInt.response}}</h1>',
       scope: $scope,
-      title: 'The fish retrieved this number for you',
+      title: titleArray[index],
       buttons: [
         {
           text: '<b>done</b>',
