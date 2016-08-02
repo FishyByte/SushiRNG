ec30f0ea32493bbd1785b334e8a22b0a9d260605
c-asakawa <chris@asakawa.me>
2016-08-02 02:08:10 -0700

    added file to post_bits to back end

---
+++ backend/post_bits.py
@@ -0,0 +1,19 @@
+import requests
+import os
+import time
+
+test_input = ''
+for x in range(0, 4):
+    path = 'data/fishBits' + str(x) + '.txt'
+    test_input = open(path, 'r').read().rstrip('\n')
+
+    payload = {
+        'secret-key': os.environ['SECRET_KEY'],
+        'raw-data': test_input
+    }
+
+    r = requests.post('https://fish-bit-hub.herokuapp.com/add-bytes', data=payload)
+    print 'status code:', r.status_code
+
+    print 'waiting 5 seconds...'
+    time.sleep(5)
